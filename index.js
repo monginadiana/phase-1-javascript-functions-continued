5bc33e4851d909120e49bf35acbdc4f95c5a5825
monginadiana <dianahmongina2@gmail.com>
2022-08-10 06:44:34 +0300

    commit tree 7f8168b5cee5ae0ad6f6192e9abaea0dfa5f4fb5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun (hiking="roller-skate"){
+   return `This Saturday, I want to ${hiking}!`;
+}
+console.log (saturdayFun())
+
+function mondayWork(work='go to the office'){
+    return `This Monday, I will ${work}.`
+
+}
+console.log (mondayWork())
+
+function wrapAdjective (ast){
+    return function active(wrap){
+        return `You are ${ast}${wrap}${ast}!`
+
+    }
+    active('a hard worker')
+    
+
+}
